230cb72899e50ba994542e80dc672dfbee651c7d
maziang <maziang@bytedance.com>
2021-08-17 18:45:44 +0800

    sampling log

--- pkg/zlog/mid.go
+++ pkg/zlog/mid.go
@@ -2,6 +2,7 @@ package zlog
 
 import (
 	"context"
+	"math/rand"
 
 	"github.com/cloudwego/kitex/pkg/endpoint"
 	"github.com/cloudwego/kitex/pkg/klog"
@@ -13,8 +14,10 @@ func LogMiddleware(mwCtx context.Context) endpoint.Middleware { // middleware bu
 
 	return func(next endpoint.Endpoint) endpoint.Endpoint { // middleware
 		return func(ctx context.Context, request, response interface{}) error {
-
-			logger.Debugf("Request is %v", request)
+			// 采样 1%
+			if rand.Intn(100) < 1 {
+				logger.Debugf("Request is %v", request)
+			}
 			err := next(ctx, request, response)
 
 			return err
